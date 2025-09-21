51929c5b372e4344ca65c47021c14df392e191d3
Anthony Ng <newyork.anthonyng@gmail.com>
2025-09-21 12:12:28 -0400

    feat: polish ui

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -28,7 +28,7 @@ export function AddTodo({ onAddTodo }: AddTodoProps) {
           value={text}
           onChange={(e) => setText(e.target.value)}
           placeholder="Add a new todo..."
-          className="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+          className="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-gray-900 placeholder-gray-500"
         />
         <button
           type="submit"
